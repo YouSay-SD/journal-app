9e94556c2d0c849fa732da70b500de7827df05ee
YouSay-SD <yousay.sd@gmail.com>
2020-08-30 15:45:23 -0300

    Added Styles For Auth Title - Input - Social Networks

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
   return (
     <>
 
-      <h3>Login</h3>
+      <h3 className="auth__title mb-4">Login</h3>
 
       <form>
 
@@ -12,23 +13,27 @@ export const LoginScreen = () => {
           type="text"
           placeholder="Email"
           name="email"
+          className="auth__input"
+          autoComplete="off"
         />
 
         <input 
           type="password"
           placeholder="Password"
           name="password"
+          className="auth__input"
         />
 
         <button
           type="submit"
+          className="btn btn-primary btn-block"
         >
           Login
         </button>
 
         <hr />
 
-        <div>
+        <div className="auth__social-networks">
 
           <p>Login with social networks</p>
 
@@ -47,10 +52,14 @@ export const LoginScreen = () => {
               <b>Sign in with google</b>
 
             </p>
-            
+
           </div>
                     
         </div>
+
+        <Link to="/auth/register">
+          Create new account
+        </Link>
 
       </form>
 
