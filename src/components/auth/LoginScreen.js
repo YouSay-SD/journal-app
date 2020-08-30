f5c2d23815701e753edf0f2dda7e924046b05bcb
YouSay-SD <yousay.sd@gmail.com>
2020-08-30 02:37:58 -0300

    Add Styles For Auth Box Container

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,8 +2,58 @@ import React from 'react'
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>Login Screen</h1>
-    </div>
+    <>
+
+      <h3>Login</h3>
+
+      <form>
+
+        <input 
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+
+        <input 
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+
+        <button
+          type="submit"
+        >
+          Login
+        </button>
+
+        <hr />
+
+        <div>
+
+          <p>Login with social networks</p>
+
+          <div 
+            className="google-btn"
+          >
+
+            <div className="google-icon-wrapper">
+
+              <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+
+            </div>
+
+            <p className="btn-text">
+
+              <b>Sign in with google</b>
+
+            </p>
+            
+          </div>
+                    
+        </div>
+
+      </form>
+
+    </>
   )
 }
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -6,17 +6,21 @@ import { LoginScreen } from '../components/auth/LoginScreen';
 export const AuthRouter = () => {
   return (
 
-    <div>
+    <div className="auth__main">
 
-      <Switch>
-  
-        <Route exact path="/auth/login" component={ LoginScreen } />
-  
-        <Route exact path="/auth/register" component={ RegisterScreen } />
-  
-        <Redirect to="/auth/login" />
-  
-      </Switch>
+      <div className="auth__box-container">
+
+        <Switch>
+    
+          <Route exact path="/auth/login" component={ LoginScreen } />
+    
+          <Route exact path="/auth/register" component={ RegisterScreen } />
+    
+          <Redirect to="/auth/login" />
+    
+        </Switch>
+
+      </div>
 
     </div>
 
