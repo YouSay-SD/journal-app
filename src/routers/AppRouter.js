9b279136188237dbda81f31b4abbeb63a49c3259
YouSay-SD <yousay.sd@gmail.com>
2020-08-30 01:24:39 -0300

    Set Base Scss

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,26 +1,33 @@
 import React from 'react'
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
-import { AuthRouter } from './AuthRouter'
-import { JournalScreen } from '../components/journal/JournalScreen'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import { AuthRouter } from './AuthRouter';
+import { JournalScreen } from '../components/journal/JournalScreen';
 
 export const AppRouter = () => {
   return (
 
-    <>
+    <Router>
 
-      <Router>
+      <div>
 
-          <Switch>
+        <Switch>
 
-            <Route path="/auth" component={ AuthRouter } />
+          <Route path="/auth" component={ AuthRouter } />
 
-            <Route exact path="/" component={ JournalScreen } />
+          <Route exact path="/" component={ JournalScreen } />
 
-          </Switch>
-          
-      </Router>
+          <Redirect to="/auth/login" />
 
-    </>
+        </Switch>
+
+      </div>
+        
+    </Router>
 
   )
 }
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { LoginScreen } from '../components/auth/LoginScreen'
-import { RegisterScreen } from '../components/auth/RegisterScreen'
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+import { LoginScreen } from '../components/auth/LoginScreen';
 
 export const AuthRouter = () => {
   return (
 
-    <>
+    <div>
 
       <Switch>
-
-        <Route path="/auth/login" component={ LoginScreen } />
-
-        <Route path="/auth/register" component={ RegisterScreen } />
-
+  
+        <Route exact path="/auth/login" component={ LoginScreen } />
+  
+        <Route exact path="/auth/register" component={ RegisterScreen } />
+  
         <Redirect to="/auth/login" />
-
+  
       </Switch>
-      
-    </>
+
+    </div>
 
   )
 }
