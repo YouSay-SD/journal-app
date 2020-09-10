4195f6fde0ef80e121106b457991cb50c567023f
YouSay-SD <yousay.sd@gmail.com>
2020-09-10 20:51:57 -0300

    Added Validation For Form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useForm } from '../../hooks/useForm'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import validator from 'validator';
 
 export const RegisterScreen = () => {
 
@@ -15,7 +16,26 @@ export const RegisterScreen = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(e);
+
+    if ( isFormValid() ) {
+      console.log('Correct Form');
+    }
+  }
+
+  const isFormValid = () => {
+
+    if ( name.trim().length === 0 ) {
+      return false;
+
+    } else if ( !validator.isEmail( email ) ) {
+      return false;
+
+    } else if ( password !== password2 || password.length < 5 ) {
+      return false;
+
+    }
+
+    return true;
   }
 
   return (
@@ -24,6 +44,10 @@ export const RegisterScreen = () => {
       <h3 className="auth__title mb-4">Register</h3>
 
       <form onSubmit={ handleRegister }>
+
+        <div className="auth__alert-error">
+          Hola mundo
+        </div>
 
         <input 
           type="text"
