4ece4395593a133ffe3d387dc8fc4e5e03bef970
YouSay-SD <yousay.sd@gmail.com>
2020-09-13 13:53:27 -0300

    Create Auth Logout Action

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch( startLogout() );
+  }
+
   return (
     <aside className="journal__sidebar">
       
@@ -15,10 +24,11 @@ export const Sidebar = () => {
 
         </h3>
 
-        <button className="btn">
-
+        <button 
+          className="btn"
+          onClick={ handleLogout }
+        >
           Logout
-
         </button>
 
       </div>
